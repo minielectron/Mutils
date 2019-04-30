4f033fbba0789a09476c70269a260725c0607a2b
minielectron <prakash009Kp@gmail.com>
2019-04-30 13:34:42 +0530

    EditTextClicker is updated to support textview also

--- mutils/src/main/java/electrophile/mutils/components/EditTextDrawableClicker.java
+++ mutils/src/main/java/electrophile/mutils/components/EditTextDrawableClicker.java
@@ -3,17 +3,18 @@ package electrophile.mutils.components;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public abstract class EditTextDrawableClicker implements View.OnTouchListener {
 
-    private EditText editText;
+    private TextView editText;
     final public static int DRAWABLE_LEFT = 0;
     final public static int DRAWABLE_TOP = 1;
     final public static int DRAWABLE_RIGHT = 2;
     final public static int DRAWABLE_BOTTOM = 3;
     private int drawablePosition;
 
-    public EditTextDrawableClicker(EditText editText,int drawablePosition) {
+    public EditTextDrawableClicker(TextView editText,int drawablePosition) {
         this.editText = editText;
         this.drawablePosition = drawablePosition;
     }
